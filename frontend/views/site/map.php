9823d305a15ae21be1935f6dd0970878efc6cf51
Gregor Doroschenko <kinggregor92@googlemail.com>
2016-10-23 15:28:30 +0200

    [FEATURE] create new filter

--- frontend/views/site/map.php
+++ frontend/views/site/map.php
@@ -8,13 +8,16 @@ use dosamigos\leaflet\layers\Marker;
 use dosamigos\leaflet\layers\TileLayer;
 use dosamigos\leaflet\LeafLet;
 use dosamigos\leaflet\widgets\Map;
+use dosamigos\leaflet\layers\GeoJson;
+use dosamigos\leaflet\plugins\markercluster\MarkerCluster;
+use yii\web\View;
 
-$this->title = 'Map';
+$this->registerAssetBundle(yii\web\JqueryAsset::className(), View::POS_HEAD);
+
+$this->title = 'Karte';
 $this->params['breadcrumbs'][] = $this->title;
 
-$center = new LatLng(['lat' => '51', 'lng' => '11']);
-
-$marker = new Marker(['latlng' => $center, 'popupContent' => 'Test! Hey!']);
+$center = new LatLng(['lat' => '51.0508900', 'lng' => '13.7383200']);
 
 $tileLayer = new TileLayer([
     'urlTemplate' => 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
@@ -29,14 +32,107 @@ $leaflet = new LeafLet([
     'center' => $center,
 ]);
 
-$leaflet->addLayer($marker)
-        ->addLayer($tileLayer);
+$jsonData = json_decode(file_get_contents(Yii::getAlias('@files').'/json/playplaces.json'));
+
+$all = new \dosamigos\leaflet\layers\LayerGroup();
+$getA = new \dosamigos\leaflet\layers\LayerGroup();
+$getB = new \dosamigos\leaflet\layers\LayerGroup();
+$getC = new \dosamigos\leaflet\layers\LayerGroup();
+$getD = new \dosamigos\leaflet\layers\LayerGroup();
+$getE = new \dosamigos\leaflet\layers\LayerGroup();
+
+$alter = new \dosamigos\leaflet\layers\LayerGroup();
+$alter2 = new \dosamigos\leaflet\layers\LayerGroup();
+$spielA = new \dosamigos\leaflet\layers\LayerGroup();
+$spielA2 = new \dosamigos\leaflet\layers\LayerGroup();
+
+foreach($jsonData->features as $feature) {
+
+    $popup = 'Ort: ' . $feature->properties->ort .
+        '<br />Spiele: ' . $feature->properties->spiel .
+        '<br /><a href="'.\yii\helpers\Url::to(['site/detail', 'id' => $feature->properties->autoid]).'" target="_blank">Mehr Info...</a>';
+    $district = preg_split('/[\s,]+/', $feature->properties->oa);
+    switch($district[1]) {
+        case 'Cotta':
+            $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+            $newMarkerA = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+            $getA->addLayer($newMarkerA);
+            break;
+        case 'Prohlis':
+            $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+            $newMarkerB = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+            $getB->addLayer($newMarkerB);
+            break;
+        case 'Neustadt':
+            $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+            $newMarkerC = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+            $getC->addLayer($newMarkerC);
+            break;
+        case 'Pieschen':
+            $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+            $newMarkerD = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+            $getD->addLayer($newMarkerD);
+            break;
+        case 'Altstadt':
+            $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+            $newMarkerE = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+            $getE->addLayer($newMarkerE);
+            break;
+    }
+    if($feature->properties->altersgr == "0 bis 6 Jahre") {
+        $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+        $newMarkerAlter = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+        $alter->addLayer($newMarkerAlter);
+    }
+    if($feature->properties->altersgr == "6 bis 12 Jahre") {
+        $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+        $newMarkerAlter2 = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+        $alter2->addLayer($newMarkerAlter2);
+    }
+    $spiel = explode(" ", $feature->properties->spiel);
+    if($spiel[0] == "Klettern") {
+        $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+        $newMarkerSpiel = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+        $spielA->addLayer($newMarkerSpiel);
+    }
+    /*
+    $getA->addLayer($newMarkerA);
+    $getB->addLayer($newMarkerB);
+    $getC->addLayer($newMarkerC);
+    $getD->addLayer($newMarkerD);
+    $getE->addLayer($newMarkerE);*/
+
+    $pos = new LatLng(['lat' => $feature->geometry->coordinates[1], 'lng' => $feature->geometry->coordinates[0]]);
+    $newMarker = new Marker(['latlng' => $pos, 'popupContent' => $popup]);
+    $all->addLayer($newMarker);
+
+}
+
+
+$layers = new \dosamigos\leaflet\controls\Layers([
+    'overlays' => [
+        'Alle' => $all,
+        'Cotta' => $getA,
+        'Prohlis' => $getB,
+        'Neustadt' => $getC,
+        'Pieschen' => $getD,
+        'Altstadt' => $getE,
+        '0 bis 6 Jahre' => $alter,
+        '6 bis 12 Jahre' => $alter2,
+        'Klettern' => $spielA,
+        //'Tischtennis' => $spielA2
+    ]
+]);
+
+$leaflet->addControl($layers);
+$leaflet->addLayer($tileLayer);
 
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>This is the About page. You may modify the following file to customize its content:</p>
-
-    <?= Map::widget(['leafLet' => $leaflet]) ?>
+    <?= Map::widget([
+        'leafLet' => $leaflet,
+        'options' => [
+            'style' => 'min-height: 600px',
+        ]
+    ]) ?>
 </div>
